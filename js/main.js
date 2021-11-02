dd0604e0599b1cfa5afec6692295a90afdec2e97
kiyo1215 <g.k.n15@icloud.com>
2021-11-02 14:42:37 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const array = [2, 4, 7, 5, 4, 3, 8];
+const result = array.filter(function (x, i, value) {
+  return value.indexOf(x) === i;
+});
+console.log(result);
+
+function leapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let lastYear = 2020;
+if(leapYear(lastYear)){
+  console.log(lastYear + "年はうるう年です");
+}else{
+  console.log(lastYear + "年はうるう年ではありません");
+}
+let thisYear = 2021;
+if(leapYear(thisYear)){
+  console.log(thisYear　+ "年はうるう年です");
+}else{
+  console.log(thisYear + "年はうるう年ではありません");
+}
